46c929b1cbbc88e50cea8fe3011f7ad6f56385ab
afds4567 <33995840+afds4567@users.noreply.github.com>
2023-09-21 22:10:21 +0900

    feat: implement refreshToken logic

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,11 +1,52 @@
 import { useEffect, useState } from 'react';
+import { DEFAULT_URL } from '../constants/constant';
 
-export async function fetchData(url: string) {
-  const response = await fetch(url);
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
+export async function fetchData(url: string, method = 'GET') {
+  const accessToken = localStorage.getItem('userToken');
+
+  const headers: any = {};
+
+  if (accessToken) {
+    headers['Authorization'] = `Bearer ${accessToken}`;
   }
-  return await response.json();
+  console.log(accessToken, method, headers);
+  console.log(url);
+  let response = await fetch(url, {
+    method: method,
+    headers: headers
+  });
+
+  console.log(response);
+  // console.log('response', response);
+  //토큰이 만료되어 401 에러가 발생하면
+  if (response.status === 401) {
+    try {
+      //토큰을 재발급 받는다.
+      const refreshResponse = await fetch(`${DEFAULT_URL}/refresh-token`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      if (!refreshResponse.ok) throw new Error('리프레시 토큰을 받지 못했습니다.');
+
+      const data = await refreshResponse.json();
+
+      // 재발급 받은 토큰을 저장한다.
+      localStorage.setItem('userToken', data.accessToken);
+
+      headers['Authorization'] = `Bearer ${data.accessToken}`;
+
+      response = await fetch(url, { method, headers, credentials: 'include' });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  if (!response.ok) throw new Error(`HTTP 에러! 상태 코드: ${response.status}`);
+
+  return response.json();
 }
 
 const useFetch = <T>(fetchFunction: (url: string) => Promise<T>, url: string): T | undefined => {
